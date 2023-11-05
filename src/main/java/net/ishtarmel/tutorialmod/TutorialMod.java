81f4e9852fe00ab5266578c7cca14873372c90fd
MelIshtar <evan.vlaar.crowe@gmail.com>
2023-11-05 14:44:27 -0600

    Added Item Cool Rock and Texture as well as formatting for textures and lang

--- src/main/java/net/ishtarmel/tutorialmod/TutorialMod.java
+++ src/main/java/net/ishtarmel/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.ishtarmel.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.ishtarmel.tutorialmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -31,6 +32,8 @@ public class TutorialMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -47,9 +50,10 @@ public class TutorialMod
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.COOLROCK);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
